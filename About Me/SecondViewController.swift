cd1f300dc62f75a6b5b0dc6142cd29c5fc2f0499
George Arias <ginxer@gmail.com>
2018-12-17 23:07:58 -0500

    Initial Commit

---
+++ About Me/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  About me
+//
+//  Created by George Arias on 12/17/18.
+//  Copyright © 2018 George Arias. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
